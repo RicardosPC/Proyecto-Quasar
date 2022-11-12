22bb8819a2a68cae329c53d4dbe50bb706240a15
RicardosPC <89116371+RicardosPC@users.noreply.github.com>
2022-11-12 07:57:04 -0600

    quita

--- src/pages/Abaout.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <q-page>
-    <h1>priea</h1>
-  </q-page>
-</template>
--- src/pages/ejemplo.vue
+++
@@ -1,5 +0,0 @@
-<!-- eslint-disable vue/multi-word-component-names -->
-
-<template>
-  <h3>Prueba a la verga</h3>
-</template>
