d13c6a77763ad27e6c62d88665a2d539b0375f50
AmayaJudith <113407547+AmayaJudith@users.noreply.github.com>
2022-11-15 22:03:35 -0600

    firestore proyecto vinculado

--- src/boot/database.js
+++ src/boot/database.js
@@ -2,10 +2,21 @@ import { boot } from 'quasar/wrappers'
 import { initializeApp } from 'firebase/app'
 import { getFirestore } from 'firebase/firestore'
 
-// "async" is optional;
-// more info on params: https://v2.quasar.dev/quasar-cli/boot-files
-// export default boot(async (/* { app, router, ... } */) => {
-// something to do
-// })
+// TODO: Replace the following with your app's Firebase project configuration
+// See: https://firebase.google.com/docs/web/learn-more#config-object
+const firebaseConfig = {
+  apiKey: 'AIzaSyDftWNp8KlXW5RH5IX6U3Ns-NC2U29yQxs',
+  authDomain: 'cellphoneebay-2af2d.firebaseapp.com',
+  projectId: 'cellphoneebay-2af2d',
+  storageBucket: 'cellphoneebay-2af2d.appspot.com',
+  messagingSenderId: '274797383550',
+  appId: '1:274797383550:web:02adc82b51c1990e2fed2f'
+}
 
-export default { initializeApp, getFirestore, boot }
+// Initialize Firebase
+const app = initializeApp(firebaseConfig)
+
+// Initialize Cloud Firestore and get a reference to the service
+const db = getFirestore(app)
+
+export default { db, boot }
