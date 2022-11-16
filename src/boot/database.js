9f3ec145b49082a249ab43eea91f26c4a8133611
AmayaJudith <113407547+AmayaJudith@users.noreply.github.com>
2022-11-15 21:47:31 -0600

    db configurada

---
+++ src/boot/database.js
@@ -0,0 +1,11 @@
+import { boot } from 'quasar/wrappers'
+import { initializeApp } from 'firebase/app'
+import { getFirestore } from 'firebase/firestore'
+
+// "async" is optional;
+// more info on params: https://v2.quasar.dev/quasar-cli/boot-files
+// export default boot(async (/* { app, router, ... } */) => {
+// something to do
+// })
+
+export default { initializeApp, getFirestore }
